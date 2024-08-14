a37936cf92425088a5271e034e3e82e0554656cf
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-08-14 23:22:49 +0530

    massive commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+
+# # Replace 'your_file.csv' with the path to your CSV file
+# df = pd.read_csv("your_file.csv")
+
+# # Display the first few rows of the dataframe
+# print(df.head())
+
+# import os
+
+# directory = "./"
+# files = os.listdir(directory)
+# print(files)
